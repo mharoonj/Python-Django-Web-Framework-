730c71819e3b0d311ee5bbbd5f93c21d1fc9d548
m.Haroon Javed <mharoon.j@hotmail.com>
2021-11-11 00:07:54 +0500

    added filters

--- pages/views.py
+++ pages/views.py
@@ -10,9 +10,12 @@ def home_view(request, *args, **kwargs):
 
 
 def contact_view(request,*args, **kwargs):
-    print("args :", args, kwargs)
-    print("request :", request.user)
+    my_context = {
+        "my_text":"this is contact us",
+        "my_number":32050,
+        "my_list":["haroon", "fareed"]
+    }
     # return  HttpResponse("<h1>Hello World</h1>") 
-    return render(request, "contact.html", {})
+    return render(request, "contact.html", my_context)
 
 
