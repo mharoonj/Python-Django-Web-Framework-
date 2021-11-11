9f07a317bb3db64829061ebaebef6f250d684108
mharoonj <mharoon.j@hotmail.com>
2021-11-11 11:32:28 +0500

    product first detail view with db

--- DjangoWeb/urls.py
+++ DjangoWeb/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view
+from products.views import product_detail_view
 urlpatterns = [
     path("", home_view, name='home'),
     path("contact/", contact_view, name='contact'),
+    path("product/", product_detail_view,name="detail_view"),
     path('admin/', admin.site.urls),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def product_detail_view(request, *args, **kwargs):
+    obj = Product.objects.get(id=1)
+    # ONE WAY
+    # context = {
+    #     "title" : obj.title,
+    #     "description": obj.description
+    # }
+
+
+    # SECOND WAY
+    context = {
+        "object":obj
+    }
+    return render(request, 'product/detail.html',context)
