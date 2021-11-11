44bdbf0a4a146e74ce94131adae690ccc752481c
mharoonj <mharoon.j@hotmail.com>
2021-11-11 15:22:27 +0500

    till django pure form.... freecodecamp 2:43:11

--- products/forms.py
+++ products/forms.py
@@ -12,6 +12,9 @@ class ProductForm(forms.ModelForm ):
         ]
 
 class RawProductionForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price = forms.DecimalField()
+    title = forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder":"Enter title"})) # with no label on the left
+    description = forms.CharField(required=False, widget=forms.Textarea(attrs={
+        "class":"new-class-name two",
+        "rows":10
+    }))  # widget to change textfield to textarea
+    price = forms.DecimalField(initial=199.99) 
